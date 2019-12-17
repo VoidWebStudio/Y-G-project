131ef696bb46842d372f169cc534e56e4a4cf4f5
VoidWebStudio <cavid580@gmail.com>
2019-12-18 00:18:41 +0400

    ready

--- js/script.js
+++ js/script.js
@@ -1,58 +1,43 @@
-var swiper = new Swiper('.swiper-container', {
-  direction: 'vertical',
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
 
 
+var options = {
+    strings: [ 'branding'], 
+    typeSpeed: 90,
+    backSpeed: 80,
+    loop:true,
+    cursorChar: '|',
+    backDelay: 5500,
+    startDelay: 1000,
 
-
-
-
-
-
-// var hoverDistort = new hoverEffect({
-//   parent: document.querySelector('.wrapper'),
-//   intensity: 0.5,
-//   image1: 'img/1.png',
-//   image2: 'img/2.png',
-//   displacementImage: 'img/4.png'
-
-// });
-
-// // Wrap every letter in a span
-// var textWrapper = document.querySelector('.hero-title');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-// $(document).ready(function() {
-
-// $('.wrapper').hover(         
-// function () {
-// anime.timeline({loop: false})
-// .add({
-// targets: '.hero-title .letter',
-// translateX: [80,0],
-// translateZ: 0,
-// opacity: [0,1],
-// easing: "easeOutExpo",
-// duration: 1400,
-// delay: (el, i) => 100 + 40 * i
-// })
-// },
-
-// function () {
-// anime.timeline({loop: false})
-// .add({
-// targets: '.hero-title .letter',
-// translateX: [0,-80],
-// opacity: [1,0],
-// easing: "easeInExpo",
-// duration: 800,
-// delay: (el, i) => 40 * i
-// });         
-// }
-// );
+  };
   
-// });
+  var typed = new Typed('.element', options);
+
+var type = {
+    strings: [ 'Yudaev'], 
+    typeSpeed: 90,
+    backSpeed: 80,
+    loop:false,
+    cursorChar: '',
+    backDelay: 5500,
+    startDelay: 1000,
+
+  };
+  
+  var typed = new Typed('.typed', type);
+
+var type2 = {
+    strings: [ 'Geravin'], 
+    typeSpeed: 90,
+    backSpeed: 80,
+    loop:false,
+    cursorChar: '|',
+    backDelay: 5500,
+    startDelay: 1000,
+
+  };
+  
+  var typed = new Typed('.typed2', type2);
+
+
+  new WOW().init();
